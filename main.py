b1bdac8bc1d90c76c5508a84a2e2e083190c3381
Marcus Kok <marcusk1699@gmail.com>
2022-02-13 21:30:06 -0500

    mobile compatibility

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request
 import os
 from dotenv import load_dotenv
+from flask_mobility import Mobility
+from flask_mobility.decorators import mobile_template
 import paho.mqtt.publish as publish
+
 
 load_dotenv()
 MQTT_SERVER = os.getenv("MQTT_SERVER") # ip address of linode instance
@@ -11,12 +14,13 @@ MQTT_PATH = "test_channel"
 app = Flask(__name__)
 
 @app.route('/', methods = ["POST", "GET"])
-def index():
+@mobile_template('{mobile/}index.html')
+def index(template):
     if request.method == "POST":
         destination = request.form.get("dest")
         publish.single(MQTT_PATH, destination, hostname=MQTT_SERVER)
         print("Published message to " + MQTT_SERVER)    
-    return render_template("index.html")
+    return render_template(template)
 
 if __name__ == "__main__":
     from waitress import serve
